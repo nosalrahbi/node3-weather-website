e40bb8238dbc8e7083522a6d9c053adea6d8131f
Noss alrahbi <nalrahbi@gmail.com>
2019-05-09 21:20:09 +0400

    Remove unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Client side javascript is working')
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#message-1')
